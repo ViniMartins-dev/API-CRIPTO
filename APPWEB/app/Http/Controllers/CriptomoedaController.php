d258bd03f73bae1c16fc9c4427dc920be3c4990f
Your Name <vinicius_apple22@outlook.com>
2025-04-25 21:07:29 -0300

    controller da view

--- APPWEB/app/Http/Controllers/CriptomoedaController.php
+++ APPWEB/app/Http/Controllers/CriptomoedaController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CriptomoedaController extends Controller
 {
-    //
+    //endpoint api
+    
+    private $urlAPI = "http://localhost:8001/api/cripto/";
+
+    public function index() {
+        $response = Http::get($this->urlAPI);
+        $data = $response->json();
+
+        return view('criptomoeda.index', ['criptos' => $data['data'] ?? [], 'message' => $data['message'] ?? '']);
+
+    }
 }
